396c24d852ffca20e31b9e8121823995d0d3e81c
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-10-25 12:00:05 -0300

    25/10 - 11:59
    created maven project

---
+++ rpgGenerator/src/main/java/br/senai/sc/rpgGenerator/RpgGeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc.rpgGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgGeneratorApplication.class, args);
+	}
+
+}
