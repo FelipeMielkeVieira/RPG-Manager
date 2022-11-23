7ad8f9dc4694e26602451182066a9ee00bcf16a7
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-11-23 16:44:10 -0300

    23/11 - 16:43
    Finalização da autenticação.
    Problema no CORS.

--- rpg-generator/src/service/userService.js
+++ rpg-generator/src/service/userService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: "http://localhost:8080",
-    headers: { 'Access-Control-Allow-Origin': "*", 'Content-Type': "application/json" }
+    headers: { 'Access-Control-Allow-Origin': "*" }
 })
 
 export default api;
