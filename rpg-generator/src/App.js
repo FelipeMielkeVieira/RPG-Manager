a93e87e592a0d124d0f06059c0cbe34921108f52
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-11-23 16:55:00 -0300

    23/11 - 16:54

--- rpg-generator/src/App.js
+++ rpg-generator/src/App.js
@@ -12,6 +12,7 @@ import HomeJogador from "./pages/homeJogador/homeJogador";
 import HomeMestre from "./pages/homeMestre/homeMestre";
 import EditPerfil from './pages/editPerfil/editPerfil';
 import PaginaDados from './pages/paginaDados/paginaDados';
+import Home from './pages/home/home';
 
 import ToggleColorMode from './service/temaProvedor';
 
@@ -23,6 +24,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login></Login>}></Route>
           <Route path='/cadastro' element={<Cadastro></Cadastro>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
           <Route path='/home/jogador' element={<HomeJogador></HomeJogador>}></Route>
           <Route path='/home/mestre' element={<HomeMestre></HomeMestre>}></Route> 
           <Route path='/perfil' element={<EditPerfil></EditPerfil>}></Route> 
---
+++ rpg-generator/src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/sidebar'
+
+const Home = () => {
+    return (
+        <Sidebar>
+            <div>Home</div>
+        </Sidebar>
+    )
+}
+
+export default Home
